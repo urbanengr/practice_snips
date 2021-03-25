08c6be6fff64b1f229928b5f1b5fd1848dfd76a1
ValerieG <urbanengr@gmail.com>
2021-03-25 11:48:39 -0400

    feat: Add size function

--- linked_list.py
+++ linked_list.py
@@ -11,8 +11,35 @@ class Node:
     def __repr__(self):
         return "<Node data: %s>" % self.data
 
+class LinkedList:
+    """ an object pointing at the 'head' on a singly linked list."""
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """ Returns the number of nodes in the list
+         Takes O(n) - linear time """
+
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+
+
+l = LinkedList()
 N1 = Node(10)
-N2 = Node(20)
-N1.next_node = N2
-print(N1)
-print(N1.next_node)
+l.head = N1
+print(l.size())
+
+#N2 = Node(20)
+#N1.next_node = N2
+#print(N1)
+#print(N1.next_node)
