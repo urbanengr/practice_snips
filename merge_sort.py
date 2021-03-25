6574ddd23e0671e49f48db10aca43203e36b68fa
ValerieG <urbanengr@gmail.com>
2021-03-25 09:58:48 -0400

    feat: Add split and merge functions

--- merge_sort.py
+++ merge_sort.py
@@ -14,4 +14,49 @@ def merge_sort(list):
     left = merge_sort(left_half)
     right = merge_sort(right_half)
 
-    return merge_sort(left, right)
+    return merge(left, right)
+
+
+def split(list):
+    """ Divide the unsorted list at midpoint into sublists.
+     Returns two sublists - left and right """
+
+    mid = len(list)//2      #use floor function to find midpoint of list
+    left = list[:mid]
+    right = list[mid:]
+
+    return left, right
+
+
+def merge(left, right):
+    """ Merges two lists, sorting them in the process
+      Returns a new merged list"""
+
+    l = []
+    i = 0           #tracks left list indices
+    j = 0           #tracks right list indices
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:      #if element in right half is greater
+            l.append(left[i])       #move to end of left half
+            i += 1
+        else:
+            l.append(right[j])      #move left element to end of right half
+            j += 1
+
+    """ Create while loops to catch uneven halves that will cause above loop to end"""
+
+    while i < len(left):            #if right half is shorter than left
+        l.append(left[i])           #assumes elements are sorted
+        i += 1
+
+    while j < len(right):        #for left half is shorter than right
+        l.append(right[j])          #assumes elements are sorted
+        j += 1
+
+    return l
+
+alist = [54, 26, 62, 45, 74, 3, 20, 25, 1]
+l = merge_sort(alist)
+print(l)
+
